c0b3903d5934f072d0c968bae2d33bd04cfe4b55
JihoonJung2 <106726917+JihoonJung2@users.noreply.github.com>
2023-12-01 14:09:18 +0900

    Update Main.java
    정지훈

--- src/Twitter/Main.java
+++ src/Twitter/Main.java
@@ -8,20 +8,12 @@ public class Main {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Signup signup = new Signup();
-					signup.setVisible(true);
+					
 					
 					Signin Signin = new Signin();
 					Signin.setVisible(true);
 					
-					WritingArticle writingArticle = new WritingArticle();
-					writingArticle.setVisible(true);
 					
-					ChangePassword changePassword = new ChangePassword();
-					changePassword.setVisible(true);
-					
-					Home home = new Home();
-					home.setVisible(true);
 					
 				} catch (Exception e) {
 					e.printStackTrace();
