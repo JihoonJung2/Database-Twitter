8745122301d08c2496b744d2edd7096a1ab7a322
JihoonJung2 <106726917+JihoonJung2@users.noreply.github.com>
2023-12-01 14:10:37 +0900

    Update Signin.java
    정지훈

--- src/Twitter/Signin.java
+++ src/Twitter/Signin.java
@@ -13,35 +13,33 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.JPasswordField;
 
 public class Signin extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField Email_textField;
-	private JTextField Password_textField;
+	private JPasswordField Password_textField;
 
 	
 	public Signin() {
 		super("Sign in");
 		setTitle("Sign in");							
-		this.setSize(350, 500);	
+		this.setSize(675, 500);	
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		
-		JLabel Email_label = new JLabel("Email");
-		
 		Email_textField = new JTextField();
 		Email_textField.setColumns(10);
-		
-		JLabel Password_label = new JLabel("Password");
-		
-		Password_textField = new JTextField();
-		Password_textField.setColumns(10);
 		
 		JButton Login_button = new JButton("LOGIN");
 		
@@ -49,49 +47,83 @@ public class Signin extends JFrame {
 			   public void actionPerformed(ActionEvent ae){
 			      String textFieldValue = Email_textField.getText();
 			      // .... do some operation on value ...
+			      Home home = new Home();
+			      setVisible(false);
+			      home.setVisible(true);
 			   }
 			});
 		
-		JLabel lblNewLabel_2 = new JLabel("LOGIN");
+		JButton SignUpBtn = new JButton("Sign Up");
+		SignUpBtn.addActionListener(new ActionListener(){
+			   public void actionPerformed(ActionEvent ae){
+			      setVisible(false);
+			      Signup Signup = new Signup();
+					Signup.setVisible(true);
+			   }
+			});
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\82107\\eclipse-workspace\\Database-Twitter\\image\\twitter-312464_640.png"));
+		
+		JLabel lblNewLabel_1 = new JLabel("See  what's happeing in the world right now");
+		lblNewLabel_1.setFont(new Font("Eras Bold ITC", Font.BOLD, 21));
+		
+		JLabel lblNewLabel_2 = new JLabel("Join us today");
+		lblNewLabel_2.setFont(new Font("Gadugi", Font.BOLD | Font.ITALIC, 16));
+		
+		Password_textField = new JPasswordField();
+		
+		JLabel lblNewLabel_3 = new JLabel("E-mail : ");
+		
+		JLabel lblNewLabel_4 = new JLabel("Password : ");
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(100)
+							.addGap(22)
+							.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 56, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(Email_textField, GroupLayout.PREFERRED_SIZE, 148, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lblNewLabel_4, GroupLayout.PREFERRED_SIZE, 68, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(Password_textField, GroupLayout.PREFERRED_SIZE, 138, GroupLayout.PREFERRED_SIZE)
+							.addGap(34)
 							.addComponent(Login_button))
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addContainerGap(33, Short.MAX_VALUE)
+							.addGap(77)
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(Email_label)
-								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-									.addComponent(Password_label)
-									.addComponent(Email_textField, GroupLayout.DEFAULT_SIZE, 215, Short.MAX_VALUE)
-									.addComponent(Password_textField))))
+								.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 647, Short.MAX_VALUE)
+								.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 148, GroupLayout.PREFERRED_SIZE)))
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(130)
-							.addComponent(lblNewLabel_2)))
-					.addContainerGap(70, Short.MAX_VALUE))
+							.addGap(199)
+							.addComponent(SignUpBtn, GroupLayout.PREFERRED_SIZE, 253, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
 		);
 		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-					.addGap(56)
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(Email_textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(Login_button)
+						.addComponent(lblNewLabel_3)
+						.addComponent(lblNewLabel_4)
+						.addComponent(Password_textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(72)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 87, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 73, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(lblNewLabel_2)
-					.addPreferredGap(ComponentPlacement.RELATED, 91, Short.MAX_VALUE)
-					.addComponent(Email_label)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(Email_textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(32)
-					.addComponent(Password_label)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(Password_textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(82)
-					.addComponent(Login_button)
-					.addGap(48))
+					.addGap(18)
+					.addComponent(SignUpBtn)
+					.addGap(109))
 		);
+		
 		contentPane.setLayout(gl_contentPane);
 	}
-
 }
