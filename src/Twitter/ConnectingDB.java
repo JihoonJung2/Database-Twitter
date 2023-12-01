c9eb3d2f02497dac4da9424f0a2259fc7c7074c7
JihoonJung2 <106726917+JihoonJung2@users.noreply.github.com>
2023-12-01 14:08:42 +0900

    Update ConnectingDB.java
    정지훈 버전

--- src/Twitter/ConnectingDB.java
+++ src/Twitter/ConnectingDB.java
@@ -5,31 +5,31 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectingDB {
-	
+	Connection con = null;
+	   private static final String JDBC_URL = "jdbc:mysql://localhost:3306/twitter";
+	    private static final String USER = "root";
+	    private static final String PASSWORD = "12345";
 	public ConnectingDB() {
-		Connection con = null;
-		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url = "jdbc:mysql://127.0.0.1:3306/Twitter";
-			String user = "root", passwd = "12345";
-			con = DriverManager.getConnection(url, user, passwd);
-			System.out.println(con);
-		} catch (ClassNotFoundException e){ 
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-			// insert 든 딜리트든 select
-			// database operations ...
-		
-			
-		try {
-			if (con != null && !con.isClosed()) con.close();
-		} catch (SQLException e){
-			e.printStackTrace();
-		}
-	}
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+            System.out.println(con);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+	public Connection getConnection() {
+        return con;
+    }
+	public void closeConnection() {
+        try {
+            if (con != null && !con.isClosed()) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 	
 }
