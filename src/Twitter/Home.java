7ce31f62c7b03c79e53544852dd30143ed78d63b
JihoonJung2 <106726917+JihoonJung2@users.noreply.github.com>
2023-12-01 14:09:03 +0900

    Update Home.java
    정지훈

--- src/Twitter/Home.java
+++ src/Twitter/Home.java
@@ -9,6 +9,9 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import javax.swing.SwingConstants;
 
 public class Home extends JFrame {
 
@@ -18,8 +21,9 @@ public class Home extends JFrame {
 	public Home() {
 		super("Home");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(350, 500);	
+		this.setSize(640, 500);	
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
@@ -29,11 +33,28 @@ public class Home extends JFrame {
 		profile_button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Profile profile = new Profile();
+				setVisible(false);
 				profile.setVisible(true);
 			}
 		
 		});
-		profile_button.setBounds(6, 6, 64, 40);
+		profile_button.setBounds(53, 169, 96, 23);
 		contentPane.add(profile_button);
+		
+		JButton btnNewButton = new JButton("Home");
+		btnNewButton.setBounds(52, 52, 80, 30);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(58, 136, 91, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(53, 202, 96, 23);
+		contentPane.add(btnNewButton_2);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(-10, 60, 50, 15);
+		contentPane.add(lblNewLabel);
 	}
 }
